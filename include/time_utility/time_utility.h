451d58d40ecc0900a59a6e1f34d663bffdc0ca68
Dup4 <lyuzhi.pan@gmail.com>
2022-06-28 23:46:45 +0800

    chore: init time_utility

---
+++ include/time_utility/time_utility.h
@@ -0,0 +1,12 @@
+#ifndef TIME_UTILITY_TIME_UTILITY_H
+#define TIME_UTILITY_TIME_UTILITY_H
+
+#include <string>
+
+namespace time_utility {
+
+class TimeUtility {};
+
+}  // namespace time_utility
+
+#endif  // TIME_UTILITY_TIME_UTILITY_H
--- test/time_utility_benchmark.cc
+++ test/time_utility_benchmark.cc
@@ -1,10 +1,7 @@
 #include <benchmark/benchmark.h>
 
-#include "time_utility/hello_world.h"
-
 static void BenchmarkHelloWorld(benchmark::State& state) {
     for (auto _ : state) {
-        HelloWorld();
     }
 }
 
